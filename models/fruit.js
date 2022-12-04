25f9e1af587063a1d42b52cd4804e4eb7af45fc7
Eric Turner <equality509@gmail.com>
2022-12-04 12:48:35 -0500

    authentification finished

--- models/fruit.js
+++ models/fruit.js
@@ -12,8 +12,9 @@ const { Schema, model } = mongoose // destructuring, grabbing model and Schema o
 const fruitsSchema = new  Schema({
     name: String,
     color: String,
-    readyToEat: Boolean
-})
+    readyToEat: Boolean,
+    username: String
+});
 
 const Fruit = model('Fruit', fruitsSchema)
 
